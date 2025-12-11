2a6e2579914a047561f386d440fa05337a49844c
sonakshi0604 <sonakshidashore2006@gmail.com>
2025-12-11 19:48:48 +0530

    after that

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 print("hi yukta")
 print("we are just getting started")
 print("hey hey hey")
-<<<<<<< HEAD
 print("lalala")
-=======
 print("Shakshiii")
->>>>>>> 2a4e88fbb6dcb974220b3101122d22a368b3cacd
+
