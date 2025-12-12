fccb0869c26a06d36e50a4f8ad90d8198171541b
sonakshi0604 <sonakshidashore2006@gmail.com>
2025-12-12 14:23:43 +0530

    Snake code updated

--- snake.py
+++ snake.py
@@ -1,1 +1,66 @@
 print("This file for code of snake!")
+
+#snake.py
+
+from turtle import Turtle
+
+STARTING_POSITIONS = [ (0,0), (-20,0), (-40,0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+class Snake :
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        t = Turtle("square")
+        t.color("white")
+        t.penup()
+        t.goto(position)
+        self.segments.append(t)
+
+    def extend(self):
+        # here we are adding a segment at the end
+        self.add_segment(self.segments[-1].position())
+
+    def move(self):
+        # moving segments from tail to head
+        for idx in range(len(self.segments)-1, 0, -1):
+            new_x = self.segments[idx-1].xcor()
+            new_y = self.segments[idx-1].ycor()
+            self.segments[idx].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+        
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def reset(self):
+        # moving old segments off-screen and recreatin snake
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+        
