344c6e31a60412400a8d82eed4c498d35076a74d
sonakshi0604 <sonakshidashore2006@gmail.com>
2025-12-12 22:47:05 +0530

    Food added

--- food.py
+++ food.py
@@ -3,17 +3,16 @@ import random
 
 class Food(Turtle):
 
-    def _init_(self):
-        super()._init_()
+    def __init__(self):
+        super().__init__()
         self.shape("circle")
         self.color("blue")
         self.penup()
-        self.shapesize(stretch_len=0.5, stretch_wid=0.5)  # small size
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.speed("fastest")
         self.refresh()
 
     def refresh(self):
-        # Pick a random x and y position
         random_x = random.randint(-280, 280)
         random_y = random.randint(-280, 280)
         self.goto(random_x, random_y)
