8465886b238abecf9493b81ea55654f2559cc397
shaks2703 <shak.see2703@gmail.com>
2025-12-12 22:11:51 +0530

    update score file

--- score.py
+++ score.py
@@ -1,1 +1,27 @@
+from turtle import Turtle
 
+ALIGNMENT = "center"
+FONT = ("Arial", 18, "normal")
+
+class Scoreboard(Turtle):
+
+    def _init_(self):
+        super()._init_()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.hideturtle()
+        self.update_score()
+
+    def update_score(self):
+        self.clear()
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.update_score()
+
+    def reset(self):
+        self.score = 0
+        self.update_score()
